4b566fe33ef2a972c42408968ab580f675eede54
Tristan Ross <tristan.ross@midstall.com>
2023-11-09 14:57:37 -0800

    feat(icc): tag reading

--- vizops/color/icc.zig
+++ vizops/color/icc.zig
@@ -4,6 +4,33 @@ const Allocator = std.mem.Allocator;
 const builtin = @import("builtin");
 const Icc = @This();
 
+fn byteSwapAllFields(comptime S: type, ptr: *S) void {
+    if (@typeInfo(S) != .Struct) @compileError("byteSwapAllFields expects a struct as the first argument");
+    inline for (std.meta.fields(S)) |f| {
+        switch (@typeInfo(f.type)) {
+            .Struct => byteSwapAllFields(f.type, &@field(ptr, f.name)),
+            .Array => {
+                const len = @field(ptr, f.name).len;
+                var i: usize = 0;
+                while (i < len) : (i += 1) {
+                    @field(ptr, f.name)[i] = @byteSwap(@field(ptr, f.name)[i]);
+                }
+            },
+            else => {
+                @field(ptr, f.name) = @byteSwap(@field(ptr, f.name));
+            },
+        }
+    }
+}
+
+fn readStructBig(reader: anytype, comptime T: type) !T {
+    var res = try reader.readStruct(T);
+    if (builtin.cpu.arch.endian() != std.builtin.Endian.Big) {
+        byteSwapAllFields(T, &res);
+    }
+    return res;
+}
+
 pub const DeviceClasses = struct {
     pub const input = "scnr";
     pub const display = "mntr";
@@ -17,7 +44,7 @@ pub const DeviceClasses = struct {
 pub const DeviceClass = std.meta.DeclEnum(DeviceClasses);
 
 pub const TagEntry = extern struct {
-    sig: @Vector(4, u8),
+    sig: [4]u8,
     off: u32,
     size: u32,
 
@@ -32,9 +59,83 @@ pub const TagEntry = extern struct {
 
         var i: usize = 0;
         while (i < count) : (i += 1) {
-            list.appendAssumeCapacity(try reader.readStructBig(TagEntry));
+            list.appendAssumeCapacity(try readStructBig(reader, TagEntry));
         }
         return list;
+    }
+};
+
+pub const LocaleUnicode = extern struct {
+    sig: [4]u8,
+    reserved: u32,
+    count: u32,
+    size: u32,
+};
+
+pub const LocaleUnicodeRecord = extern struct {
+    lang: [2]u8,
+    country: [2]u8,
+    len: u32,
+    off: u32,
+};
+
+pub const TagData = union(enum) {
+    cprt: std.StringHashMap([]const u16),
+    desc: std.StringHashMap([]const u16),
+
+    pub const Error = error{ UnsupportedSignature, BadSignature };
+
+    pub fn read(alloc: Allocator, sig: [4]u8, reader: anytype) (@TypeOf(reader).NoEofError || Allocator.Error || Error)!TagData {
+        inline for (@typeInfo(TagData).Union.fields) |f| {
+            if (f.type == std.StringHashMap([]const u16)) {
+                if (std.mem.eql(u8, &sig, f.name)) {
+                    const tbl = try readStructBig(reader, LocaleUnicode);
+                    if (!std.mem.eql(u8, &tbl.sig, "mluc")) return error.BadSignature;
+
+                    var reclist = try std.ArrayList(LocaleUnicodeRecord).initCapacity(alloc, tbl.count);
+                    defer reclist.deinit();
+
+                    var i: usize = 0;
+                    while (i < tbl.count) : (i += 1) {
+                        reclist.appendAssumeCapacity(try readStructBig(reader, LocaleUnicodeRecord));
+                    }
+
+                    var records = std.StringHashMap([]const u16).init(alloc);
+                    errdefer records.deinit();
+                    try records.ensureTotalCapacity(tbl.count);
+
+                    i = 0;
+                    while (i < tbl.count) : (i += 1) {
+                        const record = reclist.items[i];
+
+                        const key = try std.fmt.allocPrint(alloc, "{s}_{s}", .{ @as([2]u8, record.lang), @as([2]u8, record.country) });
+                        errdefer alloc.free(key);
+
+                        var buf = try alloc.alloc(u16, @divExact(record.len, @sizeOf(u16)));
+                        errdefer alloc.free(buf);
+
+                        for (buf) |*c| c.* = try reader.readInt(u16, .Big);
+
+                        records.putAssumeCapacity(key, buf);
+                    }
+
+                    if (std.mem.eql(u8, f.name, "cprt")) {
+                        return .{ .cprt = records };
+                    } else if (std.mem.eql(u8, f.name, "desc")) {
+                        return .{ .desc = records };
+                    }
+                    unreachable;
+                }
+            }
+        }
+        return Error.UnsupportedSignature;
+    }
+
+    pub fn deinit(self: TagData, alloc: Allocator) void {
+        _ = alloc;
+        switch (self) {
+            .cprt, .desc => |unicode| @constCast(&unicode).deinit(),
+        }
     }
 };
 
@@ -48,9 +149,9 @@ pub const DateTime = extern struct {
 };
 
 pub const Xyz = extern struct {
-    x: i32,
-    y: i32,
-    z: i32,
+    x: u32,
+    y: u32,
+    z: u32,
 };
 
 pub const Profile = extern struct {
@@ -65,12 +166,12 @@ pub const Header = extern struct {
     size: u32,
     sig: u32,
     version: u32,
-    _deviceClass: @Vector(4, u8),
-    colorSpace: @Vector(4, u8),
-    pcs: @Vector(4, u8),
+    _deviceClass: [4]u8,
+    colorSpace: [4]u8,
+    pcs: [4]u8,
     date: DateTime,
     magic: u32,
-    platform: @Vector(4, u8),
+    platform: [4]u8,
     flags: u32,
     manufacturer: u32,
     mode: u32,
@@ -86,7 +187,7 @@ pub const Header = extern struct {
     };
 
     pub fn read(reader: anytype) (@TypeOf(reader).NoEofError || Error)!Header {
-        const hdr = try reader.readStructBig(Header);
+        const hdr = try readStructBig(reader, Header);
 
         if (hdr.version > 0x5000000) return Error.InvalidVersion;
 
@@ -96,10 +197,9 @@ pub const Header = extern struct {
     }
 
     pub fn deviceClass(self: Header) error{InvalidClass}!DeviceClass {
-        var value: [4]u8 = self._deviceClass;
         inline for (@typeInfo(DeviceClass).Enum.fields) |f| {
             const decl = @field(DeviceClasses, f.name);
-            if (std.mem.eql(u8, decl, &value)) return @enumFromInt(f.value);
+            if (std.mem.eql(u8, decl, &self._deviceClass)) return @enumFromInt(f.value);
         }
         return Error.InvalidClass;
     }
@@ -122,9 +222,9 @@ pub const Header = extern struct {
 
 hdr: Header,
 tags: std.ArrayList(TagEntry),
-tagdata: std.ArrayList([]const u8),
+tagdata: std.ArrayList(TagData),
 
-pub fn read(alloc: Allocator, reader: anytype) (@TypeOf(reader).NoEofError || Allocator.Error || Header.Error || TagEntry.Error)!*Icc {
+pub fn read(alloc: Allocator, reader: anytype) (@TypeOf(reader).NoEofError || Allocator.Error || Header.Error || TagEntry.Error || TagData.Error)!*Icc {
     const self = try alloc.create(Icc);
     errdefer alloc.destroy(self);
 
@@ -132,15 +232,11 @@ pub fn read(alloc: Allocator, reader: anytype) (@TypeOf(reader).NoEofError || Al
     self.tags = try TagEntry.readAll(alloc, reader);
     errdefer self.tags.deinit();
 
-    self.tagdata = try std.ArrayList([]const u8).initCapacity(alloc, self.tags.items.len);
+    self.tagdata = try std.ArrayList(TagData).initCapacity(alloc, self.tags.items.len);
     errdefer self.tagdata.deinit();
 
     for (self.tags.items) |t| {
-        var buf = try alloc.alloc(u8, t.size);
-        errdefer alloc.free(buf);
-
-        assert(try reader.read(buf) == t.size);
-        self.tagdata.appendAssumeCapacity(buf);
+        self.tagdata.appendAssumeCapacity(try TagData.read(alloc, t.sig, reader));
     }
     return self;
 }
@@ -149,13 +245,16 @@ pub fn deinit(self: *Icc) void {
     const alloc = self.tags.allocator;
     self.tags.deinit();
 
-    for (self.tagdata.items) |i| alloc.free(i);
+    for (self.tagdata.items) |i| i.deinit(alloc);
     self.tagdata.deinit();
 
     alloc.destroy(self);
 }
 
 test "Check size" {
+    try std.testing.expectEqual(16, @sizeOf(LocaleUnicode));
+    try std.testing.expectEqual(12, @sizeOf(LocaleUnicodeRecord));
+
     try std.testing.expectEqual(12, @sizeOf(DateTime));
     try std.testing.expectEqual(12, @sizeOf(Xyz));
     try std.testing.expectEqual(16, @sizeOf(Profile));
